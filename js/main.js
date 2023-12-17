9507c79e23498e477e41d838d5d945e6254c3581
pollyandhiscat <pollyandhiscat@outlook.com>
2023-12-16 20:08:26 -0800

    Add 'check_for_winner()' function to determine who the winner might be after each move

--- js/main.js
+++ js/main.js
@@ -116,6 +116,7 @@ function make_move(participant, current_board) {
         while (cpu_position != '') {
 
             cpu_move = Math.floor(Math.random() * current_board.length);
+            cpu_position = current_board[cpu_move];
 
         }
 
@@ -135,6 +136,7 @@ function make_move(participant, current_board) {
         while (player_position != '') {
 
             player_move = prompt(`That space is taken. Choose another. Here is the current board: ${current_board}`);
+            player_position = current_board[player_move];
         }
 
         // Fill in the player move on the board.
@@ -168,12 +170,175 @@ function play_game(player_name) {
 
     }
 
+    const check_for_winner = () => {
+
+        /*
+
+        Checks the current board for a winner.
+        If no winner is found and the board is
+        filled up, the game is considered a tie.
+
+        */
+
+        player_character = player['player_character'];
+        computer_character = computer['computer_character'];
+        let winner = "";
+
+        const check_for_winner_by_row = () => {
+
+
+            /*
+            
+            Checks to see if either player or computer
+            has three in a row horizontally. If so, their name
+            is returned as the winner.
+
+            */
+
+            winner = "";
+
+            let row_1_array = board.slice(0, 3);
+            let row_2_array = board.slice(3, 6);
+            let row_3_array = board.slice(6, -1);
+
+            let row_1 = row_1_array.join('');
+            let row_2 = row_2_array.join('');
+            let row_3 = row_3_array.join('');
+
+            let player_win_row = player_character + player_character + player_character;
+            let computer_win_row = computer_character + computer_character + computer_character;
+
+            if (row_1 == player_win_row || row_2 == player_win_row || row_3 == player_win_row) {
+
+                winner = 'player';
+                return winner;
+            }
+
+            else if (row_1 == computer_win_row || row_2 == computer_win_row || row_3 == computer_win_row) {
+
+                winner = 'computer';
+                return winner;
+            }
+
+            else {
+
+                return winner;
+            }
+
+        }
+
+        const check_for_winner_by_column = () => {
+
+            /*
+
+            Checks to see if either player or computer
+            has three in a row vertically.
+
+            */
+
+            winner = "";
+
+            let column_1 = board[0] + board[3] + board[6];
+            let column_2 = board[1] + board[4] + board[7];
+            let column_3 = board[2] + board[5] + board[8];
+
+            let player_win_column = player_character + player_character + player_character;
+            let computer_win_column = computer_character + computer_character + computer_character;
+
+            if (column_1 == player_win_column || column_2 == player_win_column || column_3 == player_win_column) {
+
+                winner = 'player';
+                return winner;
+            }
+
+            else if (column_1 == computer_win_column || column_2 == computer_win_column || column_3 == computer_win_column) {
+
+                winner = 'computer';
+                return winner;
+            }
+
+            else {
+
+                return winner;
+            }
+
+        }
+
+        [
+            x, x, x,
+            0, 0, 0,
+            x, x, x
+        ]
+
+        const check_for_winner_by_diagonal = () => {
+
+            /*
+
+            Checks to see if either player or computer
+            has three in a row diagonally. 
+
+            */
+
+            winner = "";
+
+            let diagonal_1 = board[0] + board[4] + board[8];
+            let diagonal_2 = board[2] + board[4] + board[6];
+
+            let player_win_diagonal = player_character + player_character + player_character;
+            let computer_win_diagonal = computer_character + computer_character + computer_character;
+
+            if (diagonal_1 == player_win_diagonal || diagonal_2 == player_win_diagonal) {
+
+                winner = 'player';
+                return winner;
+            }
+
+            else if (diagonal_1 == computer_win_diagonal || diagonal_2 == computer_win_diagonal) {
+
+                winner = 'computer';
+                return winner;
+            }
+
+            else {
+
+                return winner;
+            }
+
+        }
+
+        let row_check = check_for_winner_by_row();
+
+        if (row_check != '') {
+
+            return row_check;
+        }
+
+
+        let column_check = check_for_winner_by_row();
+
+        if (column_check != '') {
+
+            return column_check;
+        }
+
+        let diagonal_check = check_for_winner_by_diagonal();
+
+        if (diagonal_check != '') {
+
+            return diagonal_check;
+        }
+
+        return winner;
+
+    }
+
     while (game_running == true) {
 
         current_turn = next_turn();
 
-        if (moves == 5) {
+        if (moves == 10) {
 
+            console.log(board);
             return;
         }
 
@@ -193,7 +358,6 @@ function play_game(player_name) {
         }
 
     }
-
 
 }
 
